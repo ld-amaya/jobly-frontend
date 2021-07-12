94a8926f4f2a1bc1b9ebd13e9b5ea60494f7e64f
lou <louamaya@me.com>
2021-07-12 12:01:25 -0700

    add company folder to handle company components, add CompanyDetails component

---
+++ src/company/CompanyDetails.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import JoblyApi from '../api'
+import JobList from '../jobs/JobList';
+
+function CompanyDetails() {
+    const [company, setCompany] = useState(null);
+    const { handle } = useParams();
+    
+    //load api
+    useEffect(() => {
+        console.log('hi lou');
+        async function getCompanyDetails() {
+            const c = await JoblyApi.getCompany(handle);
+            setCompany(c);
+        }
+        getCompanyDetails();
+    }, [handle]);
+
+    // handle no company return
+    if (!company) return (<p>loading</p>);
+    
+    return (
+        <div className='container col-md-8 mt-5'>
+            <h4 class="card-title">{company.name}</h4>
+            <div>{company.description}</div>
+            <div>Totel Employees: {company.numEmployees}</div>
+            <JobList jobs ={company.jobs} />
+        </div>
+    )
+}
+
+export default CompanyDetails;
