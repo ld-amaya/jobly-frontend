c29e00ba11c97f0f0f3e14fe9df633ded400f4f6
lou <louamaya@me.com>
2021-07-12 12:05:16 -0700

    add BrowserRouter and Router component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import Router from './routes/Router';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      Hi Lou
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>  
     </div>
   );
 }
