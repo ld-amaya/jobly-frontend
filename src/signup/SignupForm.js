75782e7bc3300afc21682891261ce73b9d487cc5
lou <louamaya@me.com>
2021-07-13 05:12:12 -0700

    create signup form, handle-not yet added

---
+++ src/signup/SignupForm.js
@@ -0,0 +1,84 @@
+import React, {useState} from 'react';
+
+function SignupForm() {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+        email:''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    return(
+        <form>
+            <div className='form-group'>
+                <label className='col-form-label' htmlFor='username'>Username</label>
+                <input
+                    onChange={handleChange}
+                    className ='form-control'
+                    type='text'
+                    id='username'
+                    name='username'
+                    value={formData.username}
+                />
+            </div>
+            <div className='form-group'>
+                <label className='col-form-label' htmlFor='password'>Password</label>
+                <input
+                    onChange={handleChange}
+                    className ='form-control'
+                    type='password'
+                    id='password'
+                    name='password'
+                    value={formData.password}
+                />
+            </div>
+            <div className='form-group'>
+                <label className='col-form-label' htmlFor='firstName'>First Name</label>
+                <input
+                    onChange={handleChange}
+                    className ='form-control'
+                    type='text'
+                    id='firstName'
+                    name='firstName'
+                    value={formData.firstName}
+                />
+            </div>
+            <div className='form-group'>
+                <label className='col-form-label' htmlFor='lastName'>Last Name</label>
+                <input
+                    onChange={handleChange}
+                    className ='form-control'
+                    type='text'
+                    id='lastName'
+                    name='lastName'
+                    value={formData.lastName}
+                />
+            </div>
+            <div className='form-group'>
+                <label className='col-form-label' htmlFor='email'>Email</label>
+                <input
+                    onChange={handleChange}
+                    className ='form-control'
+                    type='email'
+                    id='email'
+                    name='email'
+                    value={formData.email}
+                />
+            </div>
+            <div style={{textAlign:'end'}}>
+                <button type='submit' className='btn btn-primary btn-sm mt-3'>Submit</button>
+            </div>
+        </form>
+    )
+}
+
+export default SignupForm;
