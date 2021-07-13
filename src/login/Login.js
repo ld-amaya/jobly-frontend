5ecda70df9bb56ffc8f0b480550f32979de9ce9d
lou <louamaya@me.com>
2021-07-13 05:07:35 -0700

    add login handler

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import LoginForm from './LoginForm';
 
 function Login() {
     return (
-        <div>
-            This displays the login form
+        <div className='container col-md-4 mt-5'>
+            <h3 className = 'text-left'>Log In</h3>
+            <LoginForm />
         </div>
     )
 }
