c530d01d436aeb74c473b56bc7b845d5a33ff4ba
lou <louamaya@me.com>
2021-07-13 02:39:37 -0700

    add more routes for companies, jobs profile login signup

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import Navbar from './Navbar';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import CompanyDetails from '../company/CompanyDetails';
+import Companies from '../company/Companies';
+import Jobs from '../jobs/Jobs';
+import Profile from '../profile/Profile';
+import Login from '../login/Login';
+import Signup from '../signup/Signup';
 
 function Router() {
     return (
         <Switch>
-            <Route exact path='/company/:handle'>
+            <Route exact path='/companies'>
+                <Companies />
+            </Route>
+            <Route exact path='/companies/:handle'>
                 <CompanyDetails />
             </Route>
+            <Route exact path='/jobs'>
+                <Jobs />
+            </Route>
+            <Route exact path='/profile'>
+                <Profile />
+            </Route>
+            <Route exact path='/login'>
+                <Login />
+            </Route>
+            <Route exact path='/signup'>
+                <Signup />
+            </Route>
+            <Redirect to="/" />
         </Switch>
     )
 }
