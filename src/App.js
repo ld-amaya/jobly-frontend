6099b15c6c69b151e2d5e6171fdf32122abb843b
lou <louamaya@me.com>
2021-07-13 02:37:24 -0700

    add browswer router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Router from './routes/Router';
+import Navbar from './routes/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Router />
       </BrowserRouter>  
     </div>
