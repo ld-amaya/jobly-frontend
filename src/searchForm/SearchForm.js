36e50f1dc03cb6b97c5d4c79c04740d43ff131ec
lou <louamaya@me.com>
2021-07-13 02:43:57 -0700

    add searchForm component to be used by Company and Jobs component

---
+++ src/searchForm/SearchForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import JobCard from '../jobs/JobCard';
+
+function SearchForm({search}) {
+    const [formData, setFormData] = useState({
+        search:''
+    });
+    
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData(formData => ({ ...formData, [name]: value }));
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        search(formData);
+        setFormData({ search: '' });
+    }
+
+    return (
+        <form onSubmit ={handleSubmit}>
+            <div className='form-group row mt-3'>
+                    <div className ='col-md-10'>
+                        <input
+                            className='form-control'
+                            onChange={handleChange}
+                            type='text'
+                            id='search'
+                            name='search'
+                            placeholder='Search'
+                            value={formData.search}
+                        />
+                    </div>
+                    <button type='submit' className='btn btn-sm btn-primary col-md-2' >Search</button>
+                    
+                
+            </div>
+        </form>
+    )   
+}
+
+export default SearchForm;
