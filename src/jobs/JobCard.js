38eb912aa34aa9205a6f102e90da9d61a759a44a
lou <louamaya@me.com>
2021-07-14 05:47:28 -0700

    add apply job function

--- src/jobs/JobCard.js
+++ src/jobs/JobCard.js
@@ -1,26 +1,44 @@
-import React, {useEffect} from 'react';
+import React, {useContext, useEffect, useState } from 'react';
 import useAddCommas from '../hooks/useAddCommas';
+import UserContext from '../context/UserContext';
 
-function JobCard({ title, salary, equity }) {
+function JobCard({ title, salary, equity, id }) {
     const [value, putComma] = useAddCommas();
-    
+    const [applied, setApplied] = useState(false);
+    const { apply, hasApplied } = useContext(UserContext);
+
     // Convert numbers to string with comma 
     useEffect(() => {
         function convertSalary() {
             if (salary > 0) {
-            putComma(salary);        
+                putComma(salary);        
             }    
         }
         convertSalary();
     }, [salary])
     
+    // Check if user applied to the job
+    useEffect(() => {
+        function checkApplication() {
+              hasApplied(id) ? setApplied(true) : setApplied(false)    
+        }
+        checkApplication();
+    },[id])
+    
+    const handleApplication = () => {
+        apply(id)
+        setApplied(true);
+    }
+
     return (<div className="card mt-3">
         <div className='card-body'>
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title" data-testid={id}>{title}</h5>
             <p className="card-text">Salary: {value}</p>
             <p className="card-text">Equity: {equity}</p>
             <div style={{textAlign:'end'}}>
-                <button type='button' className="btn btn-primary btn-sm">Apply</button>
+                <button onClick={handleApplication} type='button' className="btn btn-primary btn-sm" disabled = {applied}>
+                    {applied ? 'Applied' : 'Apply'}
+                </button>
             </div>
         </div>
     </div>)
