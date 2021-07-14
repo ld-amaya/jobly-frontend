315ec10228739cbbcef42c50eba6a4d4e731e224
lou <louamaya@me.com>
2021-07-13 23:32:55 -0700

    add home route

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from './Navbar';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import CompanyDetails from '../company/CompanyDetails';
 import Companies from '../company/Companies';
@@ -7,7 +6,7 @@ import Jobs from '../jobs/Jobs';
 import Profile from '../profile/Profile';
 import Login from '../login/Login';
 import Signup from '../signup/Signup';
-
+import Home from '../home/Home';
 function Router() {
     return (
         <Switch>
@@ -28,6 +27,9 @@ function Router() {
             </Route>
             <Route exact path='/signup'>
                 <Signup />
+            </Route>
+            <Route expect path='/'>
+                <Home />
             </Route>
             <Redirect to="/" />
         </Switch>
