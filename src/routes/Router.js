be22aa6e785f96f30e1c6d93ddf5b2ed56d2bbd9
lou <louamaya@me.com>
2021-07-14 04:23:47 -0700

    manage routes for logged and unlogged

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React  from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import CompanyDetails from '../company/CompanyDetails';
 import Companies from '../company/Companies';
@@ -7,20 +7,13 @@ import Profile from '../profile/Profile';
 import Login from '../login/Login';
 import Signup from '../signup/Signup';
 import Home from '../home/Home';
+import UserLoggedRoute from './UserLoggedRoute';
+
 function Router() {
     return (
         <Switch>
-            <Route exact path='/companies'>
-                <Companies />
-            </Route>
-            <Route exact path='/companies/:handle'>
-                <CompanyDetails />
-            </Route>
-            <Route exact path='/jobs'>
-                <Jobs />
-            </Route>
-            <Route exact path='/profile'>
-                <Profile />
+            <Route exact path='/'>
+                <Home />
             </Route>
             <Route exact path='/login'>
                 <Login />
@@ -28,10 +21,22 @@ function Router() {
             <Route exact path='/signup'>
                 <Signup />
             </Route>
-            <Route expect path='/'>
-                <Home />
-            </Route>
-            <Redirect to="/" />
+
+            <UserLoggedRoute exact path='/companies'>
+                <Companies />
+            </UserLoggedRoute>
+            <UserLoggedRoute exact path='/companies/:handle'>
+                <CompanyDetails />
+            </UserLoggedRoute>
+            <UserLoggedRoute exact path='/jobs'>
+                <Jobs />
+            </UserLoggedRoute>
+            <UserLoggedRoute exact path='/profile'>
+                <Profile />
+            </UserLoggedRoute>
+        
+            
+            <Redirect to='/' />
         </Switch>
     )
 }
