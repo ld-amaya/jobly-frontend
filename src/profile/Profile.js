5b0cccad16daafc97a6f82d82e0b58659953b780
lou <louamaya@me.com>
2021-07-14 04:21:46 -0700

    manage profile for user update

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProfileForm from './ProfileForm';
 
 function Profile() {
     return (
-        <div>
-            This display user profile
+        <div className='container col-md-4 mt-5'>
+            <h3>Update Profile</h3>
+            <ProfileForm />
         </div>
     )
 }
