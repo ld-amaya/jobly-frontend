cdd0866533007eb0c8dcfdba3f7a46fcacdfe4d9
lou <louamaya@me.com>
2021-07-14 05:46:57 -0700

    add job application function

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import {decodeToken } from 'react-jwt';
 function App() {
   const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
-  
+  const [jobsApplied, setJobsApplied] = useState(new Set([]));
+
   // Get user details if token is triggered
   useEffect(() => {
     async function getCurrentUser() {
@@ -20,12 +21,12 @@ function App() {
           window.localStorage.setItem('token', token);
           const { username } = decodeToken(token);
           const user = await JoblyApi.getUserDetails(username);
+          setJobsApplied(new Set([user.applications]));
           setUser(user);
         } catch (e) {
           console.error('Errors found:', e);
           setUser(null);
         }
-        
       }
     }
     getCurrentUser();
@@ -72,11 +73,23 @@ function App() {
     }
   }
 
+  function hasApplied(id) {
+    return jobsApplied.has(id);
+  }
+
+  // handles job application
+  async function apply(jobID) {
+    //Check if user applied to the job
+    if (hasApplied(jobID)) return
+    await JoblyApi.apply(user.username, jobID);
+    setJobsApplied(new Set([...jobsApplied, jobID]));
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
         <UserContext.Provider
-          value={{ user, setUser, login, logout, registration }}>
+          value={{ user, setUser, login, logout, registration, apply, hasApplied }}>
           <Navbar />
           <Router />
         </UserContext.Provider>
