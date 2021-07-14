19adafbf39cb95328ceb0967fbf256cc8580bd20
lou <louamaya@me.com>
2021-07-14 04:22:48 -0700

    separate navigation menus for logged user - company jobs profile logout

--- src/routes/NavForUserLogged.js
+++ src/routes/NavForUserLogged.js
@@ -3,15 +3,15 @@ import { NavLink } from 'react-router-dom';
 import UserContext from '../context/UserContext';
 
 function NavForUserlogged() {
-    const { currentUser, logout } = useContext(UserContext);
-    console.log(currentUser);
+    const { user, logout } = useContext(UserContext);
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <NavLink className='nav-link' exact to='/'>Jobly</NavLink>     
             <NavLink className='nav-link' exact to='/companies'>Companies</NavLink>
             <NavLink className='nav-link' exact to='/jobs'>Jobs</NavLink>
             <NavLink className='nav-link' exact to='/profile'>Profile</NavLink>
-            <NavLink onClick ={logout} className='nav-link' exact to='/logout'> Logout { currentUser.username }</NavLink>
+            <NavLink onClick ={logout} className='nav-link' exact to='/logout'> Logout { user.username }</NavLink>
         </nav>
     )
 }
