88b311a2f38eaa3a8448f55cec0234bf70423477
lou <louamaya@me.com>
2021-07-14 04:20:37 -0700

    manage conversion of salary from num to string to add comma, used customized hook putcomma

--- src/jobs/JobCard.js
+++ src/jobs/JobCard.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import useAddCommas from '../hooks/useAddCommas';
 
-function JobCard({title,salary,equity}) {
+function JobCard({ title, salary, equity }) {
+    const [value, putComma] = useAddCommas();
+    
+    // Convert numbers to string with comma 
+    useEffect(() => {
+        function convertSalary() {
+            if (salary > 0) {
+            putComma(salary);        
+            }    
+        }
+        convertSalary();
+    }, [salary])
+    
     return (<div className="card mt-3">
         <div className='card-body'>
             <h5 className="card-title">{title}</h5>
-            <p className="card-text">Salary: {salary}</p>
+            <p className="card-text">Salary: {value}</p>
             <p className="card-text">Equity: {equity}</p>
             <div style={{textAlign:'end'}}>
-                <a href="#" class="btn btn-primary btn-sm">Apply</a>
+                <button type='button' className="btn btn-primary btn-sm">Apply</button>
             </div>
         </div>
     </div>)
